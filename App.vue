92438352f86d289b87567ac4b3e33e5cc6303f11
YuehanZhang-xiaohanhan <3051491908@qq.com>
2025-04-17 16:46:48 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	
+	export default {
+		globalData: {
+				           text: 'http://192.168.1.100:8088',
+									user:null
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
